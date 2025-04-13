f0d442e509aa8b343b61c333ea42c17b2a9b8d8e
poliven-abs <ul.polina.a@gmail.com>
2025-04-13 23:18:01 +0500

    separate  js

---
+++ script.js
@@ -0,0 +1,35 @@
+//dekstop
+
+const container = document.getElementById('block');
+const baseWidth = 1400;
+
+function updateScale() {
+const windowWidth = window.innerWidth;
+const minScale = 0.3;
+const maxScale = 1.5;
+
+const scale = Math.max(minScale, Math.min(maxScale, windowWidth / baseWidth));
+
+container.style.transform = `scale(${scale})`;
+}
+
+window.addEventListener('load', updateScale);
+window.addEventListener('resize', updateScale);
+
+//tablet+mobile
+
+const containerMobile = document.getElementById('blockMobile');
+const baseWidthMobile = 763;
+
+function updateScaleMobile() {
+const windowWidth = window.innerWidth;
+const minScale = 0.3;
+const maxScale = 1;
+
+const scale = Math.max(minScale, Math.min(maxScale, windowWidth / baseWidthMobile));
+
+containerMobile.style.transform = `scale(${scale})`;
+}
+
+window.addEventListener('load', updateScaleMobile);
+window.addEventListener('resize', updateScaleMobile);
